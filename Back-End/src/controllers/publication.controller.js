007847a371db4bbc4cc23780f068a1490e84e4d8
Luis Romero <135392523+ROMEROLUIS15@users.noreply.github.com>
2024-05-31 19:18:30 -0400

    Update publication.controller.js

--- Back-End/src/controllers/publication.controller.js
+++ Back-End/src/controllers/publication.controller.js
@@ -1,5 +1,6 @@
 const catchError = require('../utils/catchError')
 const Publication = require('../models/Publication')
+const User = require('../models/User')
 
 const getAll =  catchError(async(req,res) => {
     const publication = await Publication.findAll()
@@ -7,20 +8,29 @@ const getAll =  catchError(async(req,res) => {
 })
 
 const createPublication = catchError(async(req,res) => {
-    const { description, category, zone, userId } = req.body
-    const publication = await Publication.create({description, category, zone, userId})
+    const { description, category, zone, latitude, longitude, userId } = req.body
+    const publication = await Publication.create({description, category, zone, latitude, longitude, userId})
     return res.status(201).json(publication)
 })
 
-const remove = catchError(async(req,res) => {
+const getPublication = catchError(async(req,res) => {
+    const {id} = req.params
+    const publication = await Publication.findByPk(id, {include: [{
+        model: User,
+        attributes: ['name', 'email', 'phone', 'availability', 'img']
+    }]})
+    return res.json(publication)
+})
+
+const  removePublication = catchError(async(req,res) => {
     const { id } = req.params
     await Publication.destroy({where: {id}})
-    return res.status(204).json(publication)
-    //return res.sendStatus(204)
+    return res.sendStatus(204)
 })
 
 module.exports = {
     getAll,
     createPublication,
-    remove
+    getPublication,
+    removePublication
 }
