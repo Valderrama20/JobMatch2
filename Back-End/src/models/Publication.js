a1aff561e997699dbedaacbfc061c9646bff378f
Luis Romero <135392523+ROMEROLUIS15@users.noreply.github.com>
2024-05-31 19:20:25 -0400

    Update Publication.js

--- Back-End/src/models/Publication.js
+++ Back-End/src/models/Publication.js
@@ -2,19 +2,32 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../utils/connection')
 
 const Publication = sequelize.define('publication',{
+    latitude: {
+        type: DataTypes.FLOAT,
+        allowNull:false
+    },
+
+    longitude: {
+        type: DataTypes.FLOAT,
+        allowNull:false
+    },
+
     description: {
         type: DataTypes.STRING,
         allowNull: false
     },
+    
     category: {
         type: DataTypes.STRING,
         allowNull: false
     },
+    
     zone: {
         type: DataTypes.STRING,
         allowNull: false
     },
   
 })
+
 
 module.exports = Publication
